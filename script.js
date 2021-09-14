1d4c579d7ba32bc1936dfddf9dc7aefef4c86b1d
AshtonMar <ashtonmartingoliath@gmail.com>
2021-09-14 13:53:07 +0200

    update

---
+++ script.js
@@ -0,0 +1,119 @@
+let placeOfWord = document.querySelector(".word");
+
+function getRandomWord() {
+  let words = [
+    "sphere",
+    "harbor",
+    "raid",
+    "dimension",
+    "confrontation",
+    "weigh",
+    "topple",
+    "hunter",
+    "communist",
+    "upset",
+    "unity",
+    "permission",
+    "ample",
+    "trouser",
+    "resource",
+    "rehabilitation",
+    "opinion",
+    "sympathetic",
+    "percent",
+    "well",
+    "fashion",
+    "discover",
+    "version",
+    "hover",
+    "slice",
+    "premature",
+    "salmon",
+    "manner",
+    "letter",
+    "habit",
+    "bounce",
+    "tune",
+    "identification",
+    "guarantee",
+    "conglomerate",
+    "chain",
+    "basin",
+    "recovery",
+    "bathtub",
+    "care",
+    "hard",
+    "vigorous",
+    "temperature",
+    "survival",
+    "quality",
+    "dare",
+  ];
+
+  let chooseRandom = Math.floor(Math.random() * words.length);
+  let randomWordChosen = words[chooseRandom];
+  let randomWordSplit = randomWordChosen.split("");
+  let splitShuffle = shuffleWord(randomWordSplit);
+  let finalWord = splitShuffle.join("");
+
+  placeOfWord.innerHTML += finalWord;
+  document.querySelector(".answer-btn").addEventListener("click", () => {
+    let enteredGuess = document.querySelector(".entered-guess").value;
+
+    if (enteredGuess == randomWordChosen) {
+      let playersPoints =
+        JSON.parse(localStorage.getItem("playersPoints")) || 0;
+
+      playersPoints = playersPoints + 1;
+      alert(playersPoints + " Pts");
+
+      localStorage.setItem("playersPoints", JSON.stringify(playersPoints));
+      location.reload();
+    } else if (enteredGuess === "" || enteredGuess === null) {
+      let playersPoints =
+        JSON.parse(localStorage.getItem("playersPoints")) || 0;
+      playersPoints = playersPoints - 1;
+
+      alert(playersPoints + " Pts");
+      localStorage.setItem("playersPoints", JSON.stringify(playersPoints));
+      location.reload();
+    } else {
+      let playersPoints =
+        JSON.parse(localStorage.getItem("playersPoints")) || 0;
+
+      playersPoints = playersPoints - 1;
+      alert(playersPoints + " Pts");
+      localStorage.setItem("playersPoints", JSON.stringify(playersPoints));
+      location.reload();
+    }
+  });
+}
+
+function shuffleWord(randomWord) {
+  let currentIndex = randomWord.length,
+    randomIndex;
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [randomWord[currentIndex], randomWord[randomIndex]] = [
+      randomWord[randomIndex],
+      randomWord[currentIndex],
+    ];
+  }
+  return randomWord;
+}
+
+getRandomWord();
+
+let pointBtn = document.querySelector(".btns.point-btn");
+pointBtn.addEventListener("click", function showPionts() {
+  let playersPoints = JSON.parse(localStorage.getItem("playersPoints")) || 0;
+  alert(playersPoints + " Pts");
+});
+
+let restartBtn = document
+  .querySelector(".restart-btn")
+  .addEventListener("click", function restart() {
+    alert("You Have 0 Pts");
+    window.localStorage.clear();
+  });
